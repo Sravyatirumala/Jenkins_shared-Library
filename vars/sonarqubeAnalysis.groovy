0db2f0c4413998884bcb037f4735ed06a9b0206a
Sravya Tirumala <sravyatirumala@Sravyas-MacBook-Air.local>
2025-05-01 17:55:42 +0530

    changes made

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,6 +1,6 @@
 def call() {
     withSonarQubeEnv('sonar-server') {
-                    sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=youtube \
-                    -Dsonar.projectKey=youtube '''
+                    sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=youtube1 \
+                    -Dsonar.projectKey=youtube1 '''
 }
 }
