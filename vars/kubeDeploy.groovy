8935e39f1c5ad4e2039eda3c36c2f2b16b42a6a5
Sravya Tirumala <sravyatirumala@Sravyas-MacBook-Air.local>
2025-05-01 20:22:12 +0530

    added deploy file

--- vars/kubeDeploy.groovy
+++ vars/kubeDeploy.groovy
@@ -1,4 +1,13 @@
 def call() {
-    withKubeConfig(caCertificate: '', clusterName: '', contextName: '', credentialsId: 'k8\'scertificate', namespace: '', restrictKubeConfigAccess: false, serverUrl: '') 
+    withKubeConfig(
+        caCertificate: '', 
+        clusterName: '', 
+        contextName: '', 
+        credentialsId: 'k8'scertificate', 
+        namespace: '', 
+        restrictKubeConfigAccess: false, 
+        serverUrl: ''
+    ) {
         sh "kubectl apply -f deployment.yml"
     }
+}
